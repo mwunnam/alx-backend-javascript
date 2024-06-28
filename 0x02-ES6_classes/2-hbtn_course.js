078b88099829b0cff9a255ff960b0389d37c53ab
mwunnam <mwunnam@gmail.com>
2024-06-28 00:55:36 +0000

    done with tasks 0,1,2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,37 @@
+export default class HolbertonCourse {
+  constructor(name, length, student) {
+    this.name = name;
+    this.length = length;
+    this.student = student;
+  }
+  get name() {
+    return this._name;
+  }
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'Number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value;
+  }
+
+  get student() {
+    return this._studnets;
+  }
+  set students(value) {
+    if (!Array.isArray(value) || !value.every(student => typeof student ==
+          'string')) {
+      throw new TypeError('Studnet must be and array of strings');
+    }
+    this._students = value;
+  }
+}
