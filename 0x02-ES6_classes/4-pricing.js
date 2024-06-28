e79b9868c03c77e0fbd4dd7181197fc96010af1b
mwunnam <mwunnam@gmail.com>
2024-06-28 03:02:31 +0000

    done with task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency._name} (${this.currency._code})`;
+  }
+
+  covertPrice(amount, conversionRate) {
+    return (this._amount * conversionRate);
+  }
+}
