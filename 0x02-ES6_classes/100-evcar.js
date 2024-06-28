b01f784c0ff7fe3aea8255edc061ffd1d3bd14f8
mwunnam <mwunnam@gmail.com>
2024-06-28 21:30:49 +0000

    done with task 100

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,17 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this.range;
+  }
+
+  cloneCar() {
+    const { _brand, _motor, _color } = this;
+    return new Car(_brand, _motor, _color);
+  }
+}
