388bc77b834a1c8b13690315f83de6e9bd63dcbc
mwunnam <mwunnam@gmail.com>
2024-06-28 02:07:46 +0000

    correction

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class HolbertonCourse {
-  constructor(name, length, student) {
-    this.name = name;
-    this.length = length;
-    this.student = student;
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
   get name() {
@@ -27,13 +27,13 @@ export default class HolbertonCourse {
     this._length = value;
   }
 
-  get student() {
+  get students() {
     return this._studnets;
   }
 
   set students(value) {
-    if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
-      throw new TypeError('students must be an array of strings');
+    if (!Array.isArray(value)) {
+      throw new TypeError('Students must be an array of strings');
     }
     this._students = value;
-}
+  }
