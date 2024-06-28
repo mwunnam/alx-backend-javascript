d46b643ad9227f73046e47cdab44ac04f2935451
mwunnam <mwunnam@gmail.com>
2024-06-28 01:10:28 +0000

    correction on task 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,9 @@
-import ClassRoom from './0-classroom.js';
-initializeRooms => [
-  new ClassRoom(19),
-  new ClassRoom(20),
-  new ClassRoom(34),
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34)
   ];
-export default initializeRooms;
+}
