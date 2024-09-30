9e9defb60192888ad89207b03273f9a9b07271ef
mwunnam <mwunnam@gmail.com>
2024-09-30 23:21:51 +0000

    done with task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+console.log('Wecome to Holberton School, what is your name?')
+
+process.stdin.setEncoding('utf-8')
+
+process.stdin.on('data', (data) => {
+  const name = data.trim();
+  console.log(`Your name is: ${name}`);
+  process.stdin.pause();
+});
+
+process.on('exit', () => {
+  console.log('This important sofware is now closing');
+});
