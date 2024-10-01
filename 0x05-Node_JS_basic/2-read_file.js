515806982f0a348b8503408a3ee2217d6faf4a6c
mwunnam <mwunnam@gmail.com>
2024-10-01 14:05:01 +0000

    done with task 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  let data;
+
+  try {
+    data = fs.readFileSync(path, 'utf-8');
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+
+  const rows = data.split('\n').filter(row => row.trim() !== '');
+  console.log(rows);
+
+
+  if (rows.lenght < 2) {
+    console.log('Number of studnet: 0')
+    return
+  }
+
+  const studentCount = {};
+  for (const row of rows.slice(1)) {
+    const [firstName, , , field] = row.split(',');
+
+    if (field in studentCount) {
+      studentCount[field].count++;
+      studentCount[field].names.push(firstName);
+    } else {
+      studentCount[field] = {
+        count: 1,
+        names: [firstName],
+      };
+    }
+  }
+
+  const totalStudents = rows.lenght - 1;
+  console.log(`Numbers of students: ${totalStudents}`);
+
+  for (const field in studentCount) {
+    const { count, names } = studentCount[field];
+    console.log(`Number of students in ${field}: ${count}. List: ${names.join(', ')}`);
+  }
+}
+
+module.exports = countStudents;
