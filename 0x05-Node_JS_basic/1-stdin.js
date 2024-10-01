fb8064d40e3e5d27e5f4ef9ec2358c984f50f5b7
mwunnam <mwunnam@gmail.com>
2024-10-01 01:08:00 +0000

    done correction on task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-console.log('Welcome to Holberton School, what is your name?')
+console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.setEncoding('utf-8')
+process.stdin.setEncoding('utf-8');
 
 process.stdin.on('data', (data) => {
-  const name = data.trim();
-  console.log(`Your name is: ${name}`);
-  process.stdin.pause();
+ const name = data.trim();
+ console.log(`Your name is: ${name}`);
+ process.stdin.pause;
 });
 
 process.on('exit', () => {
-  console.log('This important sofware is now closing');
+  console.log('This important software is closing');
 });
