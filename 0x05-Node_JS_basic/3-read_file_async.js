4e6a154fa93e48045d9d0b67ba66b90bf1a1cd48
mwunnam <mwunnam@gmail.com>
2024-10-02 11:02:30 +0000

    done with task 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,53 @@
+const fs = require('fs').promise;
+
+/**
+ * Function to count student asychronously
+ * @param {string} path - Path to the CVS file
+ * @return {Promise} - A promise that resolves when the file is processed
+ */
+
+function countStudents(path) {
+  return fs.readFile(path, 'utf-8')
+    .then((data) => {
+      // Split the data into rwos and filter out empty rows
+      const rows = data.split('\n').filter(row => row.trim() !== '')
+
+      if (rows.length < 2) {
+        console.log('Number of student: 0');
+        return;
+      }
+
+      // Parse the CSV count Students by field
+      const studentCount = {};
+      for (const row of rows.slice(1)) {
+        const [firstName, , , field] = row.split(',');
+
+        if (field in studentCount) {
+          studentCount[field].count++;
+          studentCount[field].names.push(firstName);
+        } else {
+            studentCount[field] = {
+            count: 1,
+            names: [firstName],
+        };
+      }
+    }
+
+    // Log total number of students
+    const totalStudents = rows.lenth - 1;
+    console.log(`Number of students: ${totalStudents}`);
+
+    // Log the number of students in each field
+    for (const field in studentCount) {
+      const { count, names } = studentCount[field];
+      console.log(`Number of student in ${field}: ${count}. List:
+          ${names.join(', ')}`);
+    }
+  })
+  .catch(() => {
+    throw new Error('Cannot load the database');
+  });
+}
+
+
+module.exports = countStudents;
