a2cf426f205ec39a1e676aeb0e55c4af17c7c2b8
mwunnam <mwunnam@gmail.com>
2024-10-02 15:00:51 +0000

    correction

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,19 +1,14 @@
-/*
- * Create a simple http server
- */
-
-const http = require('http');
-
-const host = '127.0.0.1';
-const port = 1245;
+import http, { IncomingMessage, ServerResponse } from 'http';
 
 
-const app = http.createServer((req, res) => {
+const app = http.createServer((req: IncomingMessage, res: ServerResponse) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-app.listen(port, host);
+app.listen(1245, () => {
+  console.log('Server listening on port 1245);
+});
 
 module.export = app;
