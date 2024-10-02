aa44c371503b282bc3118f539be1522c6714eff8
mwunnam <mwunnam@gmail.com>
2024-10-02 14:23:06 +0000

    done with task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,15 @@
 const http = require('http');
 
+const host = '127'.0.0.1';
+const port = 1245;
+
+
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-app.listen(1245, () => {
-  console.log('server listening on port 1245');
-});
+app.listen(port, host, () => {});
 
 module.export = app;
