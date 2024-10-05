68ee44ce00839212026c98a309452850e316ca0d
mwunnam <mwunnam@gmail.com>
2024-10-05 23:33:33 +0000

    correction

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,23 +1,10 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-var */
 export default function taskBlock(trueOrFalse) {
-<<<<<<< HEAD
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    // eslint-disable-line no-unsed-vars
-    const task2 = false;
-     // eslint-disable-line no-unsed-vars
-=======
-  var task = false;
-  var task2 = true;
-
-  if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
->>>>>>> 8fecb04d09eea1b0caf7f1b3056a246db0803c2d
+    task = false;
+    task2 = true;
   }
 
   return [task, task2];
