932fdd0b5d57ac5e8b92f23668aba0766af5a71a
mwunnam <mwunnam@gmail.com>
2024-10-06 00:08:08 +0000

    correction

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -3,7 +3,7 @@ export default function getSanFranciscoDescription() {
   const budget = {
     income: '$119,868',
     gdp: '$154.2 billion',
-    capita: '$178,479'
+    capita: '$178,479',
   };
 
   return `As of ${year}, it was the seventh-highest income county in the United
