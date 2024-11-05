47aeffc068ae86cee5db51c08d7747e5a7514e56
mwunnam <mwunnam@gmail.com>
2024-11-05 14:18:22 +0000

    done with correction on task 1 of 0x05

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,8 +3,16 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
+
+  if (process.stdin.isTTY) {
+    process.exit();
+  } else {
+    process.stdin.end();
+  }
 });
 
 process.stdin.on('end', () => {
+  if (!process.stdin.isTTY) {
   console.log('This important software is now closing');
+  }
 });
