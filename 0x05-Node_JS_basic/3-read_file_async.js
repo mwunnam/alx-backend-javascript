e4f5f986ba58d242d7936a870f9ea302012527bc
mwunnam <mwunnam@gmail.com>
2024-11-06 21:55:49 +0000

    correction

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -6,45 +6,52 @@ const fs = require('fs').promises;
  * @return {Promise} - A promise that resolves when the file is processed
  */
 
-function countStudents(path) {
-  return fs.readFile(path, 'utf-8')
-    .then((data) => {
-      // Split the data into rwos and filter out empty rows
-      const rows = data.split('\n').filter((row) => row.trim() !== '');
+async function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8')
+      .then((data) => {
+        // Split the data into rwos and filter out empty rows
+        const rows = data.split('\n').filter((row) => row.trim() !== '');
 
-      if (rows.length < 2) {
-        console.log('Number of student: 0');
-        return;
-      }
-
-      // Parse the CSV count Students by field
-      const studentCount = {};
-      for (const row of rows.slice(1)) {
-        const [firstName, , , field] = row.split(',');
-
-        if (field in studentCount) {
-          studentCount[field].count += 1;
-          studentCount[field].names.push(firstName);
-        } else {
-          studentCount[field] = {
-            count: 1,
-            names: [firstName],
-          };
+        if (rows.length < 2) {
+          console.log('Number of student: 0');
+          return;
         }
-      }
 
-      // Log total number of students
-      const totalStudents = rows.length - 1;
-      console.log(`Number of students: ${totalStudents}`);
+        // Parse the CSV count Students by field
+        const studentCount = {};
+        for (const row of rows.slice(1)) {
+          const [firstName, , , field] = row.split(',');
 
-      // Log the number of students in each field
-      for (const field in studentCount) {
-        if (Object.property.hasOwnProperty.call(studentCount, field)) {
-          const { count, names } = studentCount[field];
-          console.log(`Number of student in ${field}: ${count}. List: ${names.join(', ')}`);
+          if (field in studentCount) {
+            studentCount[field].count += 1;
+            studentCount[field].names.push(firstName);
+          } else {
+            studentCount[field] = {
+              count: 1,
+              names: [firstName],
+            };
+          }
         }
-      }
-    });
+
+        // Log total number of students
+        const totalStudents = rows.length - 1;
+        console.log(`Number of students: ${totalStudents}`);
+
+        // Log the number of students in each field
+        for (const field in studentCount) {
+          if (Object.prototype.hasOwnProperty.call(studentCount, field)) {
+            const { count, names } = studentCount[field];
+            console.log(`Number of student in ${field}: ${count}. List: ${names.join(', ')}`);
+          }
+        }
+
+        resolve();
+      })
+      .catch(() => {
+        reject(new Error('Cannot load the database'));
+      });
+  });
 }
 
 module.exports = countStudents;
