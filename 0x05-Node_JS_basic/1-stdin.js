869c7d45bd47c8bd0d5426c9a73267438315ca54
mwunnam <mwunnam@gmail.com>
2024-11-06 10:43:34 +0000

    correction

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString();
-  process.stdout.write(`Your name is: ${name}\r`);
+  process.stdout.write(`Your name is: ${name}`);
 
   if (process.stdin.isTTY) {
     process.exit();
