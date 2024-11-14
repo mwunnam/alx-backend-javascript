1e391faa1ec3b8b78c205363d433c76055d5b5d9
mwunnam <mwunnam@gmail.com>
2024-11-14 13:26:01 +0100

    correction

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,8 @@
 export default function taskBlock(trueOrFalse) {
-  const task = true;
-  const task2 = false;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    const ignoreTask = false;
-    const ignoreTask2 = true;
+    return [task, task2];
   }
-
-  return [task, task2];
 }
