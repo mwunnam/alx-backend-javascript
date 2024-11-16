b612d804072722e19d79cfe9c63667f50dc4ef2d
mwunnam <mwunnam@gmail.com>
2024-11-16 17:44:39 +0000

    correction

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,15 +5,13 @@ function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
 
-  return Promise.allSettled([promise1, promise2])
-    .then((result) => {
-      result.map((result) => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value
-          : result.reason.toString(),
-      }));
-    });
-    .catch (error). new Error(error);
+  return Promise.allSettled([promise1, promise2]).then((results) => {
+    results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value
+        : result.reason.toString(),
+    }));
+  });
 }
 
 export default handleProfileSignup;
