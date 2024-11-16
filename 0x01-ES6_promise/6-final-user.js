34090a8a2644c87a60373703ba33b6ad81c4cda5
mwunnam <mwunnam@gmail.com>
2024-11-16 17:13:12 +0000

    done with task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+function handleProfileSignup(firstName, lastName, fileName) {
+  const promise1 = signUpUser(firstName, lastName);
+  const promise2 = uploadPhoto(fileName);
+
+  return Promise.allSettled([promise1, promise2])
+    .then((result) => {
+      result.map((result) => ({
+        status: result.satus,
+        value: result.status === 'fulfilled' ? result.value: result.reason,
+      }));
+    });
+}
