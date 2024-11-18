7b11afb1e56c3365c62aa43ea9a2eaad8a52c930
mwunnam <mwunnam@gmail.com>
2024-11-18 15:20:13 +0100

    correctoin

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,17 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promise1 = signUpUser(firstName, lastName);
-  const promise2 = uploadPhoto(fileName);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([promise1, promise2]).then((results) => {
-    results.map((result) => ({
-      status: result.status,
-      value:
-        result.status === 'fulfilled'
-          ? result.value
-          : result.reason.toString(),
-    }));
-  });
+  const results = await Promise.allSettled([userPromise, photoPromise]);
+
+  return results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
+  }));
 }
